920a387ba0e5dfb8e4155eb7cf955d5b4a77cddb
Michael Callahan <mcallahan0@yahoo.com>
2021-07-11 17:51:03 -0400

    Added a prompt to the initial questions const variable to ask if the user wants to add another team member. Created an init function that starts the prompt of the initial questions and added a call for the init function so that the prompt starts when the application is executed

--- index.js
+++ index.js
@@ -8,24 +8,40 @@ const intern = require('./lib/Intern');
 
 console.log('Please build your team');
 const initialQuestions = [
+
     {
         type: 'input',
-        message: `What is the team manager's name?`,
+        message: "What is the team manager's name?",
         name: 'manager-name'
     },
     {
         type: 'input',
-        message: `What is the team manager's id?`,
+        message: "What is the team manager's id?",
         name: 'manager-id'
     },
     {
         type: 'input',
-        message: `What is the team manager's e-mail?`,
+        message: "What is the team manager's e-mail?",
         name: 'manager-email'
     },
     {
         type: 'input',
-        message: `What is the team manager's office number?`,
+        message: "What is the team manager's office number?",
         name: 'manager-office'
+    },
+    {
+        type: 'list',
+        message: 'Which type of team member would you like to add?',
+        name: 'team-memeber',
+        choices: ['Engineer', 'Intern', "I don't want to add anymore team members"]
     }
+    
 ];
+
+function init() {
+
+    inquirer.prompt(initialQuestions);
+
+}
+
+init();
